1e7137458f20f14d0418516308ff71f46db49d29
mintyry <gitmintyry@gmail.com>
2023-11-28 15:05:27 -0800

    wrote index.js associations in  Models

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -6,37 +6,44 @@ const postData = [
         title: 'The Legend of Zelda for the NES',
         description: 'Complete in box, The Legend of Zelda for the NES from 1987',
         date_created: 'Novemeber 2, 2023',
-        user_id: 1
+        user_id: 1,
+        product_id: 1
     },
     {
         id: 2,
         title: 'New Nintendo Switch For Sale',
         description: 'Brand new nintendo switch for sale, box included no wear and tear',
         date_created: 'August 3, 2023',
-        user_id: 2
+        user_id: 2,
+        product_id: 2
     },
     {
         id: 3,
         title: 'Used Gameboy Advance SP',
         description: 'selling my used gamboy from when i was a kid, great condition, used but not in bad shape.  Box not included',
         date_created: 'Septemeber 12, 2023',
-        user_id: 3
+        user_id: 3,
+        product_id: 3
     },
     {
         id: 4,
         title: 'New Sealed Sega Genesis',
         description: 'New sega for sale, box never opened, still sealed, For collectors',
         date_created: 'October 29, 2023',
-        user_id: 4
+        user_id: 4,
+        product_id: 4
     },
     {
         id: 5,
         title: 'Sonic for the Sega Genesis',
         description: 'Used sonic game for the sega genesis, good condition but it is used.',
         date_created: 'December 3, 2023',
-        user_id: 1
+        user_id: 1,
+        product_id: 5
     }
 ];
+
+//*will product_id always match post id?
 
 const seedPost = () => Post.bulkCreate(postData);
 
