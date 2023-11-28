1bead178f4bbda96a6abdd8c7e3b24fb831eb46b
toum21 <pey2moo1@gmail.com>
2023-11-28 11:40:41 -0800

    updated index.js inside of seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const seedCategories = require('./category-seeds');
-// const seedPost = require('./product-seeds');
-// const seedProduct = require('./tag-seeds');
+const seedPost = require('./post-seeds');
+const seedProduct = require('./product-seeds');
 const seedUser = require('./user-seeds');
 
 const sequelize = require('../config/connection');
@@ -8,14 +8,15 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+  
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
-//   await seedProducts();
-//   console.log('\n----- PRODUCTS SEEDED -----\n');
+  await seedProduct();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
 
-//   await seedTags();
-//   console.log('\n----- TAGS SEEDED -----\n');
+  await seedPost();
+  console.log('\n----- POSTS SEEDED -----\n');
 
   await seedUser();
   console.log('\n----- USERS SEEDED -----\n');
