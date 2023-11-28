1aa333cfe18ef79aad73a2a8cad081e79d0426de
toum21 <pey2moo1@gmail.com>
2023-11-28 10:19:49 -0800

    created a post model

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize')
+
+const sequelize = require('../config/connection')
+
+class Post extends Model { }
+
+Post.init(
+    {
+        title: {
+            type: DataTypes.STRING
+        },
+        description: {
+            type: DataTypes.TEXT(5000)
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post
