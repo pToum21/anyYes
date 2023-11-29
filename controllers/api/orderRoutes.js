5a856985fa049e962cdddfe1d511d26bd091ca6b
pashag123 <pashag123@gmail.com>
2023-11-28 23:43:05 -0800

    orderRoutes updated

--- controllers/api/orderRoutes.js
+++ controllers/api/orderRoutes.js
@@ -1,7 +1,17 @@
+
+const {Order} = require('../../models');
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
-    res.status(200).json('hello')
- })
+router.post('/', async (req, res) => {
+    try {
+        const orderData = await Order.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        })
+        res.status(200).json(orderData);
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+ });
 
  module.exports = router;
