82de48b085e3291e1e699a467b05dc8956142e41
pashag123 <pashag123@gmail.com>
2023-11-30 15:33:24 -0800

    dashboardRoutes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -129,37 +129,7 @@ router.get('/consoles/:id', async (req, res) => {
 }
 );
 
-//view user dashboard
-//**add withauth back in after login page is made
-router.get('/:user_name', async (req, res) => {
-   try {
 
-      const userData = await User.findOne({
-         where: {
-            user_name: req.params.user_name
-         },
-         include: [Listing, Order]
-      });
-
-      if (!userData) {
-         return res.status(404).json({ message: 'Oh, no! User does not exist!' })
-      };
-
-      const user = userData.get({ plain: true });
-
-      res.render('userpage', {
-         user,
-         listings: user.Listings,
-         orders: user.Orders
-         // add this back in after login page is made
-         // logged_in: req.session.logged_in
-      })
-
-   } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'User could not load.' });
-   }
-})
 
 module.exports = router;
 
