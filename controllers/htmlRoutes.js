d26928f96c6953dcd98def60bb5d4be7c6a2c063
mintyry <gitmintyry@gmail.com>
2023-11-29 19:07:36 -0800

    added html route handler for consoles

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
       const listingData = await Listing.findAll({
          include: Category,
          limit: 3,
-         order: [['date_created','DESC']]
+         order: [['date_created', 'DESC']]
       });
 
       const listings = listingData.map((listing) => listing.get({ plain: true }));
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 
    } catch (error) {
       console.log('trouble rendering all listings');
-      res.status(500).json({ message: 'no listings showing' })
+      res.status(500).json({ message: 'no listings showing' });
    }
 
 })
@@ -26,38 +26,56 @@ router.get('/', async (req, res) => {
 router.get('/games', async (req, res) => {
    try {
       const gamesData = await Listing.findAll({
-         
          include: Category,
          where: {
             category_id: 2
          }
-         
       })
-      
+
       const games = gamesData.map((game) => game.get({ plain: true }))
       console.log(games)
-      res.render('games', {games})
+      res.render('games', { games })
    } catch (error) {
-      console.log(error)
+      console.log(error);
+      res.status(500).json({ message: 'no games showing' });
    }
 })
 
 router.get('/games/:id', async (req, res) => {
    try {
-      
+
       const gamesData = await Listing.findByPk(req.params.id, {
          include: User,
       })
       console.log(gamesData)
-      const games = gamesData.get({ plain: true})
+      const games = gamesData.get({ plain: true })
       console.log(games)
       res.render('game', {
          ...games
       })
    } catch (error) {
-      console.log(error)
+      console.log(error);
+      res.status(500).json({ message: 'single game is not showing' });
    }
-})
+});
+
+router.get('/consoles'), async (req, res) => {
+   try {
+      const consolesData = await Listing.findAll({
+         include: Category,
+         where: {
+            category_id: 1
+         }
+      });
+      const consoleListings = consolesData.map((individualConsole) => individualConsole.get({ plain: true }));
+      res.render('consoles', { consoleListings });
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'list of consoles is not showing' });
+      
+   }
+
+}
 
 module.exports = router;
 
