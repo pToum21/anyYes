ea965bca14c542995701514963a142e9b9072d65
Sal Gonzalez <imdone151@gmail.com>
2023-11-29 17:52:27 -0800

    added games route and games.id route

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -6,7 +6,11 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
    try {
 
-      const listingData = await Listing.findAll();
+      const listingData = await Listing.findAll({
+         include: Category,
+         limit: 3,
+         order: [['date_created','DESC']]
+      });
 
       const listings = listingData.map((listing) => listing.get({ plain: true }));
 
@@ -17,6 +21,35 @@ router.get('/', async (req, res) => {
       res.status(500).json({ message: 'no listings showing' })
    }
 
+})
+
+router.get('/games', async (req, res) => {
+   try {
+      const gamesData = await Listing.findAll({
+         include: Category,
+         where: {
+            id: 'category_name'
+         }
+      })
+      const games = gamesData.map((game) => game.get({ plain: true }))
+      res.render('games', games)
+   } catch (error) {
+      
+   }
+})
+
+router.get('games/:id', async (req, res) => {
+   try {
+      const gamesData = await Listing.findByPk(req.params.id, {
+         include: User,
+      })
+      const games = gamesData.get({ plain: true})
+      res.render('game', {
+         ...games
+      })
+   } catch (error) {
+      
+   }
 })
 
 module.exports = router;
