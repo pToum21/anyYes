e6b339a03218de004baa11a676f70fce520cbbd8
toum21 <pey2moo1@gmail.com>
2023-11-30 18:33:26 -0800

    in listing routes multer post is added

--- controllers/api/listingRoutes.js
+++ controllers/api/listingRoutes.js
@@ -1,5 +1,19 @@
 const router = require('express').Router();
 const { Listing } = require('../../models');
+const multer =require('multer')
+
+//multer setup
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage,
+})
+
+//multer route
+router.post('/file-upload', upload.single('image'),(req, res) => {
+    console.log(req.file) //req.file.buffer is what we store
+    console.log(req.body.title)
+    res.end();
+})
 
 // posts the listing
 router.post('/', async (req, res) => {
