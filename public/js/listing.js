7777a169f5d94736e76ee5f9e4c572191240b210
toum21 <pey2moo1@gmail.com>
2023-12-03 21:03:48 -0800

    made it so the user can only upload accepted image files

--- public/js/listing.js
+++ public/js/listing.js
@@ -29,6 +29,30 @@ const addListing = async () => {
     console.log(isSpecialEdition)
 
 
+    if (fileInput.files.length > 0) {
+        const fileName = fileInput.files[0].name;
+        const fileExtension = fileName.split('.').pop().toLowerCase();
+
+        // Array of allowed image file extensions
+        const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+
+        if (allowedExtensions.indexOf(fileExtension) === -1) {
+
+            alert("Please upload a valid image file (jpg, jpeg, png, gif).");
+            return;
+        }
+
+        // Append the image file to FormData
+        formData.append('image', fileInput.files[0]);
+    } else {
+
+        alert("Please select an image file.");
+        return;
+    }
+
+
+
     formData.append('image', fileInput.files[0]);
 
     // Append other form fields to the FormData object
