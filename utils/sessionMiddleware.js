edbe690b98c15a62fb6c237c349f8db9f1729fd6
toum21 <pey2moo1@gmail.com>
2023-12-04 17:10:29 -0800

    the redirect once logged out to login doesnt seem to be working

--- utils/sessionMiddleware.js
+++ utils/sessionMiddleware.js
@@ -1,7 +1,10 @@
 module.exports = (req, res, next) => {
+    console.log("Session ID:", req.session.id);
+    console.log("Session Expiry:", req.session.cookie.expires);
     // Check if the session cookie exists and if it's expired
     if (req.session && req.session.cookie && req.session.cookie.expires < Date.now()) {
       // Session expired, redirect to login page
+      console.log("Redirecting to login...");
       return res.redirect('/login');
     }
     // Continue to the next middleware
