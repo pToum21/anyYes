abc49c9a61a0d05130a8e5901b38780d8d71166c
toum21 <pey2moo1@gmail.com>
2023-12-05 21:40:47 -0800

    updated local storgae so once an itme is bought it is removed from local storgae and the cart

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,1 +1,14 @@
+const cart = JSON.parse(localStorage.getItem('myCart'))
+console.log(cart)
+const url = new URL(window.location.href)
 
+const params = new URLSearchParams(url.search)
+
+
+const listingId = parseInt(params.get('listing_id'))
+console.log(listingId)
+console.log(typeof listingId)
+const filteredCart = cart.filter(item => item.id !== listingId);
+console.log(filteredCart)
+
+localStorage.setItem('myCart', JSON.stringify(filteredCart))
