166837458d5511a06f92a67382d35a19905a7790
mintyry <gitmintyry@gmail.com>
2023-12-05 20:59:59 -0800

    made modal

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -31,3 +31,56 @@ function searchForItems(event) {
     const userInput = searchBar.value;
     window.location.assign(`/?q=${userInput}`);
 };
+
+//modal js
+const loginNav = document.querySelector('#login-nav');
+const xClose = document.querySelector('.delete')
+const loginModal = document.querySelector('.modal');
+
+loginNav.addEventListener('click', (event) => {
+    // Functions to open and close a modal
+    event.preventDefault();
+      loginModal.classList.add('is-active');
+});
+
+xClose.addEventListener('click', (event) => {
+    event.preventDefault();
+    loginModal.classList.remove('is-active')
+})
+  
+//     function closeModal(loginModal) {
+//         loginModal.classList.remove('is-active');
+//     }
+  
+//     function closeAllModals() {
+//       (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+//         closeModal($modal);
+//       });
+//     }
+  
+//     // Add a click event on buttons to open a specific modal
+//     (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+//       const modal = $trigger.dataset.target;
+//       const $target = document.getElementById(modal);
+  
+//       $trigger.addEventListener('click', () => {
+//         openModal($target);
+//       });
+//     });
+  
+//     // Add a click event on various child elements to close the parent modal
+//     (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+//       const $target = $close.closest('.modal');
+  
+//       $close.addEventListener('click', () => {
+//         closeModal($target);
+//       });
+//     });
+  
+//     // Add a keyboard event to close all modals
+//     document.addEventListener('keydown', (event) => {
+//       if (event.code === 'Escape') {
+//         closeAllModals();
+//       }
+//     });
+//   });
