b8c27a27efff41e15e199e96b69645ad8e0f45b6
toum21 <pey2moo1@gmail.com>
2023-12-06 17:57:23 -0800

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
-const sessionMiddleware = require('./utils/sessionMiddleware');
+// const sessionMiddleware = require('./utils/sessionMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
