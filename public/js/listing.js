1267c89a62e0c5adbc3a857853158564b0e296d0
toum21 <pey2moo1@gmail.com>
2023-12-07 13:19:02 -0800

    formatted the checkout hbs file

--- public/js/listing.js
+++ public/js/listing.js
@@ -6,7 +6,7 @@ const addListing = async (event) => {
         // Get the file input and create a FormData object
         const fileInput = document.querySelector('#myFile');
         const formData = new FormData();
-    
+
         // Put values into variables
         const title = document.querySelector('#title').value.trim();
         const description = document.querySelector('#description').value.trim();
@@ -19,7 +19,7 @@ const addListing = async (event) => {
         const color = document.querySelector('#clr-drop').value.trim();
         const isSpecialEditionInput = document.querySelector('input[name="is_special_edition"]:checked');
         const isSpecialEdition = isSpecialEditionInput ? isSpecialEditionInput.value.trim() : '';
-    
+
         // Check if any of the required fields are blank or not valid
         if (
             title === '' ||
@@ -40,56 +40,56 @@ const addListing = async (event) => {
             alert("Please fill out all required fields with valid values.");
             return; // Stop further execution
         }
-    
+
         // Check if a file is selected
         if (fileInput.files.length > 0) {
             const fileName = fileInput.files[0].name;
             const fileExtension = fileName.split('.').pop().toLowerCase();
-    
+
             // Array of allowed image file extensions
             const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-    
+
             // Check if the file extension is allowed
             if (allowedExtensions.indexOf(fileExtension) === -1) {
                 alert("Please upload a valid image file (jpg, jpeg, png, gif).");
                 return; // Stop further execution
             }
-    
+
             // Append the image file to FormData
             formData.append('image', fileInput.files[0]);
         } else {
             alert("Please select an image file.");
             return; // Stop further execution
         }
-    
+
         // Append other form fields to the FormData object
         formData.append('title', title);
         formData.append('description', description);
         formData.append('category_id', category);
-    
+
         // If category selected matches number, item value from the user will go into the correct column, leave the other null
         if (category === 1) {
             formData.append('console_name', item);
         } else if (category === 2) {
             formData.append('game_name', item);
         }
-    
+
         formData.append('console_brand', brand);
         formData.append('year', year);
         formData.append('condition', condition);
         formData.append('price', price);
         formData.append('color', color);
         formData.append('is_special_edition', isSpecialEdition);
-    
+
         // Send the form data to the server using fetch
         const response = await fetch('/api/listings/file-upload', {
             method: 'POST',
             body: formData,
         });
-    
+
         // Check if the response is successful
         if (response.ok) {
-    
+
             location.reload()
         } else {
             console.log('Error on listing.js');
@@ -98,7 +98,7 @@ const addListing = async (event) => {
         console.log(error)
     }
     // Prevent the default form submission behavior
-   
+
 };
 
 
